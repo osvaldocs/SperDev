55b0b2cc773cf274f7ba6156ae4b296d45ff1376
Sebitaslow <mazoosebas@gmail.com>
2025-08-25 08:59:41 -0500

    Database connection established

---
+++ coderBooster/backend/db.js
@@ -0,0 +1,16 @@
+// Here the central connection to MySQL will be made
+
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10
+});
+
+export default pool;
