82774f5bb1bf48022461844a15168aad5766daad
SebitasDown <areizasebas31@gmail.com>
2025-08-26 23:20:28 -0500

    add

--- coderBooster/frontend/features/auth/login.js
+++ coderBooster/frontend/features/auth/login.js
@@ -4,4 +4,18 @@ const API_URL = "http://localhost:3000/auth";
 
 export function LoginUser () {
     const form = document.getElementById("login-form");
-}
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value; 
+        try {
+            const res = await post(API_URL, { email, password });
+
+            if (res.message === "login successful") {
+                localStorage.setItem("user", JSON.stringify(res.user));
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+        }
+    });
+};
