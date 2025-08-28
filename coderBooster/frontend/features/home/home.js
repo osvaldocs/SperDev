1ae7cfe217dbe747ce87670cae70bdf93aff36f1
Sebitaslow <mazoosebas@gmail.com>
2025-08-28 07:00:30 -0500

    fixes

--- coderBooster/frontend/features/home/home.js
+++ coderBooster/frontend/features/home/home.js
@@ -1,6 +1,7 @@
 import { get, post, update, deletes } from "../../service/api";
 import { navigate } from "../../router/router.js";
 const urlSearch = "http://localhost:3000/search";
+const urlVideos = "http://localhost:3000/videos";
 
 export function homeUsers() {
   const searchBtn = document.getElementById("searchbtn");
@@ -11,13 +12,17 @@ export function homeUsers() {
     const searchInput = document.getElementById("searchInput");
     const q = searchInput.value.trim();
     try {
-        const result = await get (`${urlSearch}?q=${encodeURIComponent(q)}`)
-        resultsContainer.innerHTML = result.map(item => `
-            <div class="result-card p-2 border-bottom">
-            <h6>${item.title}</h6>
-             <video src="${item.Url}" controls width="300" height="180" style="border-radius: 8px;"></video>
-          </div>
-`);
+    const result = await get(`${urlSearch}?q=${encodeURIComponent(q)}`);
+    const resultVideos = await get(urlVideos);
+    resultsContainer.innerHTML = result.map(item => {
+      const videoMatch = resultVideos.find(video => video.title === item.title);
+      return `
+        <div class="result-card p-2 border-bottom">
+          <h6>${item.title}</h6>
+          <video src="${videoMatch?.Url || ''}" controls width="300" height="180" style="border-radius: 8px;"></video>
+        </div>
+      `;
+    })
     } catch (error) {
         console.error("Error fetching search results:", error);
     }
